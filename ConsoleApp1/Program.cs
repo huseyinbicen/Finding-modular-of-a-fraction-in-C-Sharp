50a0fdd0d169bba7858a105dafbfd55ad137621a
Hüseyin Biçen <huseynbicen@outlook.com>
2019-05-12 23:24:06 +0300

    master

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,8 +8,36 @@ namespace ConsoleApp1
 {
     class Program
     {
+        /// <summary>
+        /// The problem is what is the result of 77/9 % 17 = ??  
+        /// So, 77/9 Mod 17 = ??
+        /// Numerator = 77
+        /// Denominator = 9
+        /// Mod = 17
+        /// </summary>
         static void Main(string[] args)
         {
+            
+            Console.WriteLine(FractionalMod(77, 9, 17)); 
+            Console.ReadKey();
+        }
+
+        public static int FractionalMod(int Numerator, int Denominator, int Mod)
+        {
+            int r = 0;
+            
+            for (int i = 1; i < 100; i++)
+            {
+                if ((Numerator + i * Mod)%Denominator == 0)
+                {
+                    Numerator += i * Mod;
+                    Console.WriteLine("i degeri = " + i);
+                    break;
+                }
+            }
+            Numerator /= Denominator;
+            r = Numerator % Mod;
+            return r;
         }
     }
 }
